2b25840abdd0803a7ab4aa989cab41d19fd0a54e
48984228328Aninha <Amanda_rodrigues.de.siqueira@hotmail.com>
2026-03-11 18:13:18 -0300

    chore: add dependencies

---
+++ src/main/java/com/farmacia/kusuriya/KusuriyaApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.kusuriya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusuriyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KusuriyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmacia/kusuriya/KusuriyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.kusuriya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusuriyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
